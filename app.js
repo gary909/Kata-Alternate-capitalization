2445ff77ae3941b626557c57ea45d54268fe7917
gary909 <garywhite61@hotmail.com>
2023-05-15 12:59:37 +0200

    code

---
+++ app.js
@@ -0,0 +1,8 @@
+function capitalize(s){
+    return [];
+};
+
+console.log(capitalize("abcdef")); // 'AbCdEf', 'aBcDeF'
+console.log(capitalize("codewars")); // 'CoDeWaRs', 'cOdEwArS'
+console.log(capitalize("abracadabra")); // 'AbRaCaDaBrA', 'aBrAcAdAbRa'
+console.log(capitalize("codewarriors")); // 'CoDeWaRrIoRs', 'cOdEwArRiOrS'
