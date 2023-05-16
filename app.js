e13b3467b18eca5e436f36f3a7e044c9db0a0029
gary909 <garywhite61@hotmail.com>
2023-05-16 14:07:18 +0200

    code working

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 function capitalize(s){
-    return [];
+    let myStr1 = "";
+    let myStr2 = "";
+
+    for(let i = 0; i < s.length; i++){
+        if(i % 2 == 0){
+            myStr1 = myStr1 + s[i].toUpperCase();
+        } else {
+            myStr1 = myStr1 + s[i];
+        }
+    }
+
+    for(let i = 0; i < s.length; i++){
+        if(i % 2 == 1){
+            myStr2 = myStr2 + s[i].toUpperCase();
+        } else {
+            myStr2 = myStr2 + s[i];
+        }
+
+    }
+
+    return [myStr1, myStr2];
 };
 
 console.log(capitalize("abcdef")); // 'AbCdEf', 'aBcDeF'
